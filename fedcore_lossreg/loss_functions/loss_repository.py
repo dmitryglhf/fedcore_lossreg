8ea0abac7ec2936cac429e9fa7222ab57395d800
Dmitry Gilemkhanov <dima.rize@yandex.ru>
2025-03-16 18:57:18 +0300

    fix: LaiLoss small fix

--- fedcore_lossreg/loss_functions/loss_repository.py
+++ fedcore_lossreg/loss_functions/loss_repository.py
@@ -8,25 +8,22 @@ class LaiLoss(nn.Module):
         self.alpha = alpha
         self.base_loss = base_loss()
 
-    def forward(self, y_true, y_pred, compute_grad_fn=None):
+    def forward(self, y_true, y_pred):
         base_loss_value = self.base_loss(y_true, y_pred)
 
-        # Compute gradient and then k_i
-        if y_pred.requires_grad:
-            temp_loss = self.base_loss(y_true, y_pred)
-            grads = torch.autograd.grad(temp_loss, y_pred, create_graph=True)[0]
-            k_i = torch.mean(torch.abs(grads))
-        else:
-            k_i = torch.tensor(1.0)  # default value?
+        # Compute gradient scalar
+        temp_loss = self.base_loss(y_true, y_pred)
+        grads = torch.autograd.grad(temp_loss, y_pred, create_graph=True)[0]
+        k_i = torch.mean(torch.abs(grads))
 
         # Lai regularization
         lai_func = None
         if self.alpha >= 1:
-            a = torch.abs(torch.tensor(k_i)) / (torch.sqrt(1 + k_i**2))
+            a = torch.abs(k_i) / (torch.sqrt(1 + k_i**2))
             b = self.alpha / (torch.sqrt(1 + k_i**2))
             lai_func = torch.max(a, b)
         elif self.alpha < 1:
-            a = torch.abs(torch.tensor(k_i)) / (self.alpha * torch.sqrt(1 + k_i**2))
+            a = torch.abs(k_i) / (self.alpha * torch.sqrt(1 + k_i**2))
             b = 1 / (torch.sqrt(1 + k_i**2))
             lai_func = torch.max(a, b)
 
