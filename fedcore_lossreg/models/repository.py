57bb02f9c944864e6ba9eea896e0b3e43d99aa6b
Dmitry Gilemkhanov <dima.rize@yandex.ru>
2025-03-16 11:13:19 +0300

    work: added l2 regr model

--- fedcore_lossreg/models/repository.py
+++ fedcore_lossreg/models/repository.py
@@ -1,7 +1,10 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+import seaborn as sns
 
+
+torch.manual_seed(42)
 
 class MLP(nn.Module):
     def __init__(self, input_size, output_size, 
@@ -20,8 +23,12 @@ class MLP(nn.Module):
         self.loss_func = loss_func(**loss_kwargs)  
         self.optimizer = optimizer(self.parameters(), **opt_kwargs)
 
-    def fit(self, num_epochs: int = 100, train_dl: DataLoader = None):
+    def fit(self, num_epochs: int = 100,
+            train_dl: DataLoader = None,
+            plot_loss: bool = False):
         self.train()
+        loss_array = []
+        epoch_array = []
         for epoch in range(num_epochs):
             loss = 0.0
             for x_batch, y_batch in train_dl:
@@ -33,6 +40,11 @@ class MLP(nn.Module):
 
             if (epoch + 1) % 10 == 0:
                 print(f'Epoch {epoch+1}/{num_epochs}, Loss: {loss:.4f}')
+                loss_array.append(loss.item())
+                epoch_array.append(epoch+1)
+        print('Fitting complete!')
+        if plot_loss:
+            plot_loss_curve(epoch_array, loss_array)
 
     def predict(self, X_test):
         self.eval()
@@ -44,9 +56,50 @@ class MLP(nn.Module):
 
 
 class L2Regression(nn.Module):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, output_size, 
+                 loss_func=nn.MSELoss, 
+                 optimizer=torch.optim.Adam, 
+                 loss_kwargs={}, 
+                 opt_kwargs={'lr': 1e-4}):
         super().__init__()
         self.linear = nn.Linear(input_size, output_size)
+        self.loss_func = loss_func(**loss_kwargs)  
+        self.optimizer = optimizer(self.parameters(),
+                                   **opt_kwargs,
+                                   weight_decay=0.01)
+
+    def fit(self, num_epochs: int = 100,
+            train_dl: DataLoader = None,
+            plot_loss: bool = False):
+        self.train()
+        alpha = 0.01
+        loss_array = []
+        epoch_array = []
+        for epoch in range(num_epochs):
+            for x_batch, y_batch in train_dl:
+                self.optimizer.zero_grad()
+                pred = self(x_batch)
+                loss = self.loss_func(pred, y_batch)
+                loss.backward()
+                self.optimizer.step()
+
+            if (epoch + 1) % 10 == 0:
+                print(f'Epoch {epoch+1}/{num_epochs}, L2 loss: {loss:.4f}')
+                loss_array.append(loss.item())
+                epoch_array.append(epoch+1)
+        print('Fitting complete!')
+        if plot_loss:
+            plot_loss_curve(epoch_array, loss_array)
+
+    def predict(self, X_test):
+        self.eval()
+        preds = self(X_test)
+        return preds.detach().numpy()
 
     def forward(self, x):
         return self.linear(x)
+
+
+def plot_loss_curve(epoch_array, loss_array):
+    print('Plotting the loss curve:')
+    sns.lineplot(x=epoch_array, y=loss_array)
--- fedcore_lossreg/preprocessing/convert.py
+++ fedcore_lossreg/preprocessing/convert.py
@@ -4,11 +4,11 @@ from torch.utils.data import DataLoader, TensorDataset
 
 
 def convert_df_to_dl(X_train, y_train: pd.DataFrame,
-                     batch_size: int = 64,
+                     batch_size: int = 128,
                      shuffle: bool = True):
     X_train = X_train.to_numpy()
     y_train = y_train.to_numpy()
     X_train = torch.tensor(X_train, dtype=torch.float32)
-    y_train = torch.tensor(y_train, dtype=torch.float32)
+    y_train = torch.tensor(y_train, dtype=torch.float32).unsqueeze(-1)
     train_ds = TensorDataset(X_train, y_train)
     return DataLoader(train_ds, batch_size, shuffle=shuffle)
