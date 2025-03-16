fix: LaiLoss gradient update

fedcore_lossreg/loss_functions/loss_repository.py
@@ -8,9 +8,18 @@ class LaiLoss(nn.Module):
         self.alpha = alpha
         self.base_loss = base_loss()
 
-    def forward(self, y_true, y_pred, k_i=1):
+    def forward(self, y_true, y_pred, compute_grad_fn=None):
         base_loss_value = self.base_loss(y_true, y_pred)
 
+        # Compute gradient and then k_i
+        if y_pred.requires_grad:
+            temp_loss = self.base_loss(y_true, y_pred)
+            grads = torch.autograd.grad(temp_loss, y_pred, create_graph=True)[0]
+            k_i = torch.mean(torch.abs(grads))
+        else:
+            k_i = torch.tensor(1.0)  # default value?
+
+        # Lai regularization
         lai_func = None
         if self.alpha >= 1:
             a = torch.abs(torch.tensor(k_i)) / (torch.sqrt(1 + k_i**2))